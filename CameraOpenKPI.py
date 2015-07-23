4b7d934b12f075894747ee6b4f6e52fb60a11210
thornbirdblue <thornbird.blue@gmail.com>
2015-07-23 21:43:52 +0800

    Scan Camera log is OK

--- CameraOpenKPI.py
+++ CameraOpenKPI.py
@@ -28,6 +28,11 @@ import sys,os,re,string
 # app log pattern
 appLogType = r'app_log_'
 
+# camera open start end end log
+# camera startPreview start and end log
+CamLog = (r'openCamera E',r'openCamera X',r'StartPreview E',r'StartPreview X')
+
+
 # save log record
 logs = []
 
@@ -38,14 +43,79 @@ debugLogLevel=(0,1,2,3)	# 0:no log; 1:op logic; 2:op; 3:verbose
 class AppLogType:
 	'adb log dir info'
 	logCnt = 0
-	
-	def __init__(self,name):
-		self.dirname = name
-		AppLogType.logCnt+=1
+	__path = ''
+	__dir = ''
+	__file = ''
+	__time = ['','','','','','','','','','','','','','','','']			### !!! 8 pair time record must more than len(CamLog)!!!
 
-	def ScanCameraLog(self,file):
-		if debugLog >= debugLogLevel[2]:
-			print "Parse file: "+file
+	def __init__(self,path,dir,file):
+		self.__path = path
+		self.__dir = dir
+		self.__file = file
+		AppLogType.logCnt+=1
+	
+	def __ScanCamLog(self,fd):
+		if debugLog >= debugLogLevel[1]:
+			print 'INFO: begin -------------------------------------> scan camera log!'
+
+		while 1:
+			line = fd.readline()
+			
+			if not line:
+				print 'INFO: Finish read file!\n'
+				break;
+
+			if debugLog >= debugLogLevel[2]:
+				print 'INFO: Read line is :'+line
+		
+			for i in range(0,len(CamLog)):
+				if debugLog >= debugLogLevel[2]:
+					print 'INFO: Camera log-> '+CamLog[i]
+
+				log = re.compile(CamLog[i])
+		
+				if debugLog >= debugLogLevel[2]:
+					print 'INFO: Scan log-> '+log.pattern
+
+				search = re.search(log,line)
+				if search:
+					if debugLog >= debugLogLevel[1]:
+						print 'INFO: Search Camera log->'+search.group()
+					
+					timeFormat = re.compile(r'\s\d{2}:\d{2}:\d{2}.\d+')
+					
+					if debugLog >= debugLogLevel[2]:
+						print 'INFO: TimeFormat-> '+timeFormat.pattern
+					
+					time = re.search(timeFormat,line)
+					if time:
+						if debugLog >= debugLogLevel[2]:
+							print 'INFO: Find key time-> '+time.group()
+						
+						self.__time[i] = time.group()
+						
+						if debugLog >= debugLogLevel[1]:
+							print 'INFO: Time -> '+self.__time[i]
+	
+
+	def ScanCameraLog(self):
+		if debugLog >= debugLogLevel[1]:
+			print 'Parse file: '+os.path.join(self.__path,self.__file)
+		try:
+			fd = open(os.path.join(self.__path,self.__file),'r')	
+			
+			if debugLog >= debugLogLevel[2]:
+				print 'INFO: open file :'+os.path.join(self.__path,self.__file)
+
+			self.__ScanCamLog(fd)
+
+			fd.close()
+
+		except IOError:
+			print "open file ERROR: Can't open"+os.path.join(self.__path,self.__file)
+			sys.exit()
+
+			
 		
 # Only exec in one adb_log directory!!! Can't have two adb_log dir
 def ScanFiles(arg,dirname,files):
@@ -64,11 +134,11 @@ def ScanFiles(arg,dirname,files):
 			path,name = os.path.split(dirname)
 
 			if debugLog >= debugLogLevel[2]:
-				print "Found file: "+name
+				print '\nFound Dir: '+name
 			
-			log = AppLogType(name)
+			log = AppLogType(dirname,name,file)
 			logs.append(log)
-			log.ScanCameraLog(name)
+			log.ScanCameraLog()
 
 def ParseArgv():
 	if len(sys.argv) > appParaNum+1:
@@ -85,9 +155,9 @@ def ParseArgv():
 					if type(debug) == int:
 						global debugLog
 						debugLog = debug						
-						print "Log level is: "+str(debugLog)
+						print 'Log level is: '+str(debugLog)
 					else:
-						print "cmd para ERROR: "+sys.argv[i+1]+" is not int num!!!"
+						print 'cmd para ERROR: '+sys.argv[i+1]+' is not int num!!!'
 				else:
 					CameraOpenKPIHelp()
 					sys.exit()
